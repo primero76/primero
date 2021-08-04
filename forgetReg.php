e8dc87b15524fec76929bff0a7101b9b8c751619
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 22:20:27 +0500

    Update forgetReg.php

--- forgetReg.php
+++ forgetReg.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-
-    include('index.php');
    //Get Heroku ClearDB connection information
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $cleardb_server = $cleardb_url["host"];
