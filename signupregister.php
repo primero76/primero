48f91dc82926cdade933d9119b7881ef7ed0b095
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 13:45:30 +0500

    Update signupregister.php

--- signupregister.php
+++ signupregister.php
@@ -1,8 +1,15 @@
 <?php
-    require_once 'connection.php';
     session_start();
-    header('location:login.php');
-    
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     $emailAdd = $_POST['emailinp'];
     $password = $_POST['passwordinp'];
     $mobNum = $_POST['mobNuminp'];
@@ -21,5 +28,7 @@
 
     $insquery = "insert into signup (F_NAME,M_NAME,L_NAME,USERNAME,PASSWORD,EMAIL,MOBILENUM,CITY,GENDER,DOB,AGE) values ('$f_name','$m_name','$l_name','$username','$password','$emailAdd','$mobNum','$city','$gender','$dateOfBirth','$age')";
     mysqli_query($conn,$insquery);
+    header('location:login.php');
+    
 ?>
 
