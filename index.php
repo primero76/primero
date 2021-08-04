7207aff56849ce2db99995e407d19dba336a0c40
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 07:25:21 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <link rel="icon" href="AIRLINE-LOGO2.png" type="image/gif">
     <link rel="stylesheet" href="homepage.css">
     <link rel="stylesheet" href="masterkey.css">
+    <link rel="stylesheet" href="footer.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">        
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -38,7 +43,7 @@
 
     <div class="row">
         <h1 class="subheading"> Welcome to Our Airlines </h1>
-        <p >Primero Avionics is proud to be one of the youngest global airlines to serve all six continents, and thanks to our customers’ response to our offerings, we are also the world’s fastest-growing airline. We connect more than 130 destinations on the map every day, with a fleet of the latest-generation aircraft, and an unrivalled level of service from our home and hub, the Five-star airport, Hamad International Airport in Doha, the State of Qatar.
+        <p class="intro">Primero Avionics is proud to be one of the youngest global airlines to serve all six continents, and thanks to our customers’ response to our offerings, we are also the world’s fastest-growing airline. We connect more than 130 destinations on the map every day, with a fleet of the latest-generation aircraft, and an unrivalled level of service from our home and hub, the Five-star airport, Hamad International Airport in Doha, the State of Qatar.
 
             As aviation faces its greatest challenge, we remain committed to ensuring the highest standards of safety and hygiene on board our aircraft at all times. Primero Avionics is the first global airline in the world to achieve the prestigious 5-Star COVID-19 Airline Safety Rating by international air transport rating organisation, Skytrax. This follows HIA’s success as the first airport in the Middle East and Asia to be awarded a Skytrax 5-Star COVID-19 Airport Safety Rating. These recognitions provides assurance to passengers across the world that airline health and safety standards are subject to the highest possible standards of professional, independent scrutiny and assessment.
             
@@ -53,7 +58,7 @@
             Primero Avionics
         </p>
         <h1>Section 02</h1>
-        <p>Primero Avionics is proud to be one of the youngest global airlines to serve all six continents, and thanks to our customers’ response to our offerings, we are also the world’s fastest-growing airline. We connect more than 130 destinations on the map every day, with a fleet of the latest-generation aircraft, and an unrivalled level of service from our home and hub, the Five-star airport, Hamad International Airport in Doha, the State of Qatar.
+        <p class="intro">Primero Avionics is proud to be one of the youngest global airlines to serve all six continents, and thanks to our customers’ response to our offerings, we are also the world’s fastest-growing airline. We connect more than 130 destinations on the map every day, with a fleet of the latest-generation aircraft, and an unrivalled level of service from our home and hub, the Five-star airport, Hamad International Airport in Doha, the State of Qatar.
 
             As aviation faces its greatest challenge, we remain committed to ensuring the highest standards of safety and hygiene on board our aircraft at all times. Primero Avionics is the first global airline in the world to achieve the prestigious 5-Star COVID-19 Airline Safety Rating by international air transport rating organisation, Skytrax. This follows HIA’s success as the first airport in the Middle East and Asia to be awarded a Skytrax 5-Star COVID-19 Airport Safety Rating. These recognitions provides assurance to passengers across the world that airline health and safety standards are subject to the highest possible standards of professional, independent scrutiny and assessment.
             
@@ -68,7 +73,7 @@
             Primero Avionics
         </p>
         <h1 class="sec03">Section 03</h1>
-        <p>Primero Avionics is proud to be one of the youngest global airlines to serve all six continents, and thanks to our customers’ response to our offerings, we are also the world’s fastest-growing airline. We connect more than 130 destinations on the map every day, with a fleet of the latest-generation aircraft, and an unrivalled level of service from our home and hub, the Five-star airport, Hamad International Airport in Doha, the State of Qatar.
+        <p class="intro">Primero Avionics is proud to be one of the youngest global airlines to serve all six continents, and thanks to our customers’ response to our offerings, we are also the world’s fastest-growing airline. We connect more than 130 destinations on the map every day, with a fleet of the latest-generation aircraft, and an unrivalled level of service from our home and hub, the Five-star airport, Hamad International Airport in Doha, the State of Qatar.
 
             As aviation faces its greatest challenge, we remain committed to ensuring the highest standards of safety and hygiene on board our aircraft at all times. Primero Avionics is the first global airline in the world to achieve the prestigious 5-Star COVID-19 Airline Safety Rating by international air transport rating organisation, Skytrax. This follows HIA’s success as the first airport in the Middle East and Asia to be awarded a Skytrax 5-Star COVID-19 Airport Safety Rating. These recognitions provides assurance to passengers across the world that airline health and safety standards are subject to the highest possible standards of professional, independent scrutiny and assessment.
             
@@ -83,6 +88,64 @@
             Primero Avionics
         </p>
         </div>
+
+        <div class="footer-wave">
+           
+            <div class="wave-decoration">
+              <div class="wave02">
+                  <svg version="1.1" width="100%" height="131" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100% 131.2" style="enable-background:new 0 0 100% 131.2;" xml:space="preserve">
+                       <style type="text/css">
+                          .st0 {
+                          opacity: 0.25;
+                          enable-background: new;
+                          }
+                          .st1 {
+                          opacity: 0.35;
+                          enable-background: new;
+                          }
+    
+                          
+                       </style>
+                       <path class="st0" d="M-0.5,83.4c59.6,40.5,193.3,35,316.7-12.3C525.6-9.2,756.7-9.6,979.8,12.3s445.6,57.9,669.8,54.1C1821.1,63.5,1932,56,2000,53c0,36,0,76.4,0,76.4H1L-0.5,83.4z"></path>
+                       <path class="st1" d="M309.2,46.1c265.1-57.8,453.7-19.6,687.9,6.8c285.1,32.2,564.2,63,863.4,33.4c94-9.3,119.5-19.6,139.5-19c0,32.2,0,63,0,63H0v-66C0,64.3,152.7,80.2,309.2,46.1z"></path>
+                       <path class="st1" d="M344.5,54.9c82.3-26.3,167.2-46,253-36.5S767,51.6,851.9,67.8c272.3,52,522.5,16.7,819.3,5c90-3.5,293.8-13.6,328.8-12.6c0,24,0,71,0,71H-1v-59C-1,72.3,198.7,101.6,344.5,54.9z"></path>
+                       <path d="M1731.8,97.1c-289.3,18.5-590.4,17.2-873.9-16.8C746,66.9,641.1,42.1,528.5,39.5s-249.3,31-353.7,69.9c-57.5,21.4-164.7,2.3-175.7-4.7c0,8,0,26.5,0,26.5h2003v-58C2002,73.3,1854.2,89.2,1731.8,97.1z"></path>
+                  </svg>
+                </div>
+            </div>
+        </div>
+    
+        <div class="footer-dark">
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>Services</h3>
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Book Flights</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>About</h3>
+                            <ul>
+                                <li><a href="#">Company</a></li>
+                                <li><a href="#">Team</a></li>
+                                <li><a href="#">Careers</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 item text">
+                            <h3>XYZ Airlines</h3>
+                            <p>XYZ is the largest airline, flying to 158 destinations in 85 countries. XYZ Airline operates 269 aircraft and is the world's largest operator of the Airbus A380 and Boeing 777 family of aircraft.</p>
+                        </div>
+                        <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                    </div>
+                    <p class="copyright">Company Name © 2018</p>
+                </div>
+            </footer>
+        </div>
+
 </body>
 
 </html>
