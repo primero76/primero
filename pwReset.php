09ffeea70f03470a905caebe2c0c8b8bdb984c9d
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 14:36:42 +0500

    Update pwReset.php

--- pwReset.php
+++ pwReset.php
@@ -1,13 +1,21 @@
 <?php
     session_start();
-    $con = mysqli_connect('localhost','root');
-    mysqli_select_db($con,'flight_booking');
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     $email = $_SESSION['mail'];
     $pw = $_POST['passwordinp'];
 
     $query1 = "update signup set PASSWORD='$pw' where EMAIL ='$email' ";
-    mysqli_query($con,$query1);
+    mysqli_query($conn,$query1);
 
     header("location:login.php");
 ?>
