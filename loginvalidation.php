7f66385ce3f5252e4aaee2f3245a83cc43ba0428
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 14:11:19 +0500

    Update loginvalidation.php

--- loginvalidation.php
+++ loginvalidation.php
@@ -1,11 +1,22 @@
 <?php
-    require_once 'connection.php';
     session_start();
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    
+
     $emailAdd = $_POST['emailinp'];
     $password = $_POST['passwordinp'];
 
     $s  = "select * from signup where EMAIL =  '$emailAdd' && PASSWORD = '$password' ";
-    $result = mysqli_query($con,$s);
+    $result = mysqli_query($conn,$s);
     $num = mysqli_num_rows($result);
 
     if ($num==1){
