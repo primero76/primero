4d54fca09024bc91240fd6b92b7fada4abe952ce
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 22:28:01 +0500

    Update forgetReg.php

--- forgetReg.php
+++ forgetReg.php
@@ -22,7 +22,7 @@ session_start();
 
     $generator";
     $headers = "From: airways.primero@gmail.com";
-    smtp_mailer($to_email,$subject ,$body);
+    use index.php/smtp_mailer($to_email,$subject ,$body);
 	
     $_SESSION['mail'] = $to_email;
     $_SESSION['code'] = $generator;    
