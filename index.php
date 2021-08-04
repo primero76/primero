b7840777d3a05afabbca3cb588b2eb09392cb18c
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 21:49:17 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,30 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+include('smtp/PHPMailerAutoload.php');
+ echo smtp_mailer('ashhadrider2@gmail.com','HEllo Jaani','Saala');
+ function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "airways.primero@gmail.com";
+	$mail->Password = "primero12345";
+	$mail->SetFrom("airways.primero@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+}
 header('location:homepage.html');
 ?>
