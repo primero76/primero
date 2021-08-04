35269323e4508f0d9ca9af1605846828cdbe9ef3
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 20:40:11 +0500

    Update forgetReg.php

--- forgetReg.php
+++ forgetReg.php
@@ -24,12 +24,37 @@
     $generator";
     $headers = "From: airways.primero@gmail.com";
     include('smtp/PHPMailerAutoload.php');
-    
-    if (smtp_mailer($to_email,$subject,$body)){
+    $html='Msg';
+    function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "airways.primero@gmail.com";
+	$mail->Password = "primero12345";
+	$mail->SetFrom("airways.primero@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+    smtp_mailer($to_email,$subject,$body);
     $_SESSION['mail'] = $to_email;
-    $_SESSION['code'] = $generator;
-    }
-    
+    $_SESSION['code'] = $generator;    
 ?>
 
 <!DOCTYPE html>
