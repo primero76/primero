f3277e9c3131e9a75257e1a9fefde65de2db9c11
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-04 14:33:35 +0500

    Update seatValidation.php

--- seatValidation.php
+++ seatValidation.php
@@ -1,5 +1,18 @@
 <?php
-$sql  = "select * from seats";
+
+     session_start();
+
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $sql  = "select * from seats";
     $res = mysqli_query($conn,$sql);
 
     $sql2  = "select passenger from details";
