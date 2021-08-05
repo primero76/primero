34dc9e202a89e857bbb1596dd3bbc7681abb6c54
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-06 04:10:48 +0500

    Create stylemail.php

---
+++ stylemail.php
@@ -0,0 +1,47 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "airways.primero@gmail.com";
+	$mail->Password = "primero12345";
+	$mail->SetFrom("airways.primero@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+$msg = '
+  <p style='border: 1px solid green;color: lightseagrean;'>Here are the birthdays upcoming in August!</p>
+  <br>
+  <p style='border: 1px solid green;background-color:purple;'>Here are the birthdays upcoming in August!</p>
+';
+smtp_mailer('airways.primero@gmail.com','MY HTML', $msg);
+
+?>
