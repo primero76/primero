188c1a8baf391cf1a4fa0ea9bc189ada0fa60ab9
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-06 03:48:46 +0500

    Update mailcode.php

--- mailcode.php
+++ mailcode.php
@@ -13,10 +13,6 @@ session_start();
     $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     $to_email = $_POST['emailinp'];
-
-    $sub = "Password Reset";
-    $body = "  Hi, This is Primero Avioinics, we have encounter a password reset request.If it is you then Enter the following code to reset your password <> $generator <>";
-
 function smtp_mailer($to,$subject, $msg){
 	$mail = new PHPMailer(); 
 	$mail->SMTPDebug  = 3;
@@ -46,7 +42,7 @@ function smtp_mailer($to,$subject, $msg){
 }
 $generator = rand(4321,9999);
 
-smtp_mailer($_POST['emailinp'],$sub,"Hi, This is Primero Avioinics, we have encounter a password reset request.If it is you then Enter the following code to reset your password <> $generator <>");
+smtp_mailer($_POST['emailinp'],'Password Reset',"Hi, This is Primero Avioinics, we have encounter a password reset request.If it is you then Enter the following code to reset your password <> $generator <>");
     $_SESSION['mail'] = $to_email;
     $_SESSION['code'] = $generator;   
 header('location:forgetReg.php');
