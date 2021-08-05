8db31546355a2fa9cb8691770e25c2272a9b1b50
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-06 03:29:36 +0500

    Create mailcode.php

---
+++ mailcode.php
@@ -0,0 +1,54 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+session_start();
+   //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+    $generator = rand(4321,9999);
+
+    $to_email = $_POST['emailinp'];
+
+    $sub = "Password Reset";
+    $body = "  Hi, This is Primero Avioinics, we have encounter a password reset request.If it is you then,
+    Enter the following code to reset your password <> $generator <>";
+
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "airways.primero@gmail.com";
+	$mail->Password = "primero12345";
+	$mail->SetFrom("airways.primero@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+smtp_mailer( $_POST['emailinp'],$sub,$body);
+    $_SESSION['mail'] = $to_email;
+    $_SESSION['code'] = $generator;   
+header('location:forgetReg.php');
+?>
