85dd91ad060e1227a47a98b42b5bed0c99a71b58
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-07 18:18:49 +0500

    Update index.php

--- index.php
+++ index.php
@@ -39,12 +39,4 @@ function smtp_mailer($to,$subject, $msg){
 	}
 }
 header('location:homepage.html');
-<?  
-if ((time() - $_SESSION['last_activity']) > 180) // 30* 60 = 1800
-{  
-   header("Location: logout.php");  
-} else {
-   $_SESSION['last_activity'] = time();
-}
-?>
 ?>
