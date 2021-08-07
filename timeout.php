c7249f63c2fb65c11e2f5c2e4058beece96bcf99
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-07 18:48:35 +0500

    Update timeout.php

--- timeout.php
+++ timeout.php
@@ -1,8 +1,14 @@
 <?php
-if ((time() - $_SESSION['last_activity']) > 180) // 30* 60 = 1800
-{  
-   header("Location: logout.php");  
-    } else {
-   $_SESSION['last_activity'] = time();
-    }
+session_start();
+if ($_SESSION['mail'])
+{
+   if ((time() - $_SESSION['last_activity']) > 180) // 30* 60 = 1800
+   {  
+      header("location:logout.php");  
+   } 
+   else 
+   {  
+      $_SESSION['last_activity'] = time();
+   }
+}
 ?>
