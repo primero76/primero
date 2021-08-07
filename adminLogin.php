3d68006647850dc70335c8ecc14c0f09b785dcdb
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-08 02:14:39 +0500

    Update adminLogin.php

--- adminLogin.php
+++ adminLogin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title> LOGIN </title>
+    <title> ADMIN LOGIN </title>
     <link rel="icon" href="AIRLINE-LOGO2.png" type="image/gif">
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="masterkey.css">
