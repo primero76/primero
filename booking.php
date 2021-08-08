3200ff5d0e105f860841465809d729700f35501a
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-08 14:07:52 +0500

    Update booking.php

--- booking.php
+++ booking.php
@@ -15,7 +15,7 @@
     if ($_SESSION['mail'])
     {
     $sql  = "select * from signup where EMAIL =  '$emailAdd' ";
-    $res = mysqli_query($con,$sql);
+    $res = mysqli_query($conn,$sql);
     $row = mysqli_fetch_assoc($res);
 
     $fullName = $row["F_NAME"]." ".$row["M_NAME"]." ".$row["L_NAME"];
