457e2543bbfc0a395b31bdf1652e35e6c652c481
primero76 <88411478+primero76@users.noreply.github.com>
2021-08-08 15:40:07 +0500

    Update validation.php

--- validation.php
+++ validation.php
@@ -12,14 +12,24 @@
     // Connect to DB
     $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-    $timing = $_GET['depTime'];
+    $ID = $_GET['ID'];
+    $num = rand(100000000000000,999999999999999);
+    $trackingId = $num;
+    
+    $query1 = "SELECT * FROM details where DETAILS_ID='$ID';"
+    $result = mysqli_query($conn,$query1);
+    $row = mysqli_fetch_assoc($result);
     $from = $_SESSION['from'];
     $to = $_SESSION['to'];
     $date = $_SESSION['date'];
-    $insquery2 = "UPDATE flightdata SET FLY_TIME='$timing' WHERE FLY_FROM = '$from' and FLY_TO = '$to' and FLY_ON = '$date'";
-    
+    $time = $row['departuretime'];
+    $flight = $row['flightname'];
+
+    $insquery2 = "UPDATE flightdata SET TRACKING_ID='$trackingId',FLY_TIME='$time',FLIGHT_NAME='$flight' WHERE FLY_FROM = '$from' and FLY_TO = '$to' and FLY_ON = '$date'";
     mysqli_query($conn,$insquery2);
 
-    $_SESSION['timing'] = $timing;
+    $_SESSION['time'] = $time;
+    $_SESSION['name'] =  $flight;
+    $_SESSION['track'] = $trackingId;
     header("location:seatSelection.php");
 ?>
