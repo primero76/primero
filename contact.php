015b86bff518454c324a49308cc1ed5b3cb61203
primero76 <88411478+primero76@users.noreply.github.com>
2021-09-19 16:50:28 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -17,7 +17,7 @@ session_start();
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
+    <title>Contact Us - Primero Avionics</title>
     <link rel="icon" href="AIRLINE-LOGO2.png" type="image/gif">    
     <link rel="stylesheet" href="contact.css">
     <link rel="stylesheet" href="masterkey.css"/>    
