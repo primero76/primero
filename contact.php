74fad414212092a0a7d78092c6a45117cc450ab5
primero76 <88411478+primero76@users.noreply.github.com>
2021-09-19 16:22:48 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -54,7 +54,7 @@ if (isset($_SESSION['mail']))
                     <i id="cross" class="fa fa-times"></i>
                 </label>
                 <ul>
-                    <li><i class="fa fa-home"></i><a href="index.php"> About Us </a></li>
+                    <li><i class="fa fa-home"></i><a href="homepage.php"> About Us </a></li>
                     <li><i class="fa fa-newspaper-o"></i><a onclick="alert('Please login first');"> Book </a></li>
                     <li><i class="fa fa-tasks"></i><a href="tracking.php"> Manage</a></li>
                     <li><i class="fa fa-address-book"></i><a onclick="alert('Already on contact Us page')"> Contact Us</a></li>
