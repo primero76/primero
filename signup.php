f7093234f05bc2fa4f4316bfa752e1fb048d6e5f
primero76 <88411478+primero76@users.noreply.github.com>
2021-09-19 17:34:28 +0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD']=="POST")
         $gender = test_input($_POST['genderinp']);
     } 
     $m_name = test_input($_POST['m_nameinp']);
-}
+    
     function test_input($data) 
     {
       $data = trim($data);
@@ -56,8 +56,7 @@ if ($_SERVER['REQUEST_METHOD']=="POST")
       $data = htmlspecialchars($data);
       return $data;
     }
-if ($_SERVER['REQUEST_METHOD']=="POST")
-{
+
     if ($emailAdd && $password && $mobNum && $f_name && $m_name && $l_name && $city && $gender)
     { 
         $sql = "select * from signup where pEmail = '$emailAdd'";
